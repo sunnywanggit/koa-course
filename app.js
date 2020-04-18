f103234c6cc6beb3e2387eef7ce2508293329f47
ergouwang <mrergouwang@gmail.com>
2020-04-18 21:49:01 +0800

    yarn add koa-router

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const Koa = require('koa')
+//安装json模块
+const json = require('koa-json')
 
-const app = new Koa()
+
+app = new Koa()
+
+//使用json模块
+app.use(json())
 
 app.use(async ctx => {
     ctx.body = {msg:"hello koa!"}
@@ -8,4 +14,5 @@ app.use(async ctx => {
 
 
 app.listen(3000)
+
 
