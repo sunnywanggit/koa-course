50c829b2d9751f4ec0aa05f2631844ade51dc38b
ergouwang <mrergouwang@gmail.com>
2020-04-18 22:15:54 +0800

    yarn add koa-ejs

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const Koa = require('koa')
 //安装json模块
 const json = require('koa-json')
+const KoaRouter = require('koa-router')
 
 
-app = new Koa()
+const app = new Koa()
+const router = new KoaRouter()
+
 
 //使用json模块
 app.use(json())
 
-app.use(async ctx => {
-    ctx.body = {msg:"hello koa!"}
-});
+// app.use(async ctx => {
+//     ctx.body = {msg:"hello koa!"}
+// });
+
+router.get('/test',ctx=>(ctx.body = 'hello router'))
+
+//配置路由模块
+app.use(router.routes()).use(router.allowedMethods())
+
 
 
 app.listen(3000)
